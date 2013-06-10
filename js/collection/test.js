c86d605d3fe7d480ea88a02c39f73d5ca42a4d02
Alex Sexton <alexsexton@gmail.com>
2013-06-10 17:51:34 -0500

    adding in data and connections for the app

---
+++ js/collection/test.js
@@ -0,0 +1,10 @@
+define(function( require ) {
+  var Backbone = require('backbone');
+  var TestModel = require('model/test');
+
+  return Backbone.Collection.extend({
+    name: 'TestCollection',
+    model: TestModel,
+    url: '/data/metadata.json'
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-require({
+// Quick shims and setup to make urls and external
+// libs happy
+requirejs.config({
   baseUrl: '/js/',
   shim: {
     'backbone': {
@@ -9,6 +11,33 @@ require({
       exports: '_'
     }
   }
-},['backbone', 'jquery'], function ( Backbone, $) {
-  
+});
+
+// Application starting point
+define(function( require ) {
+
+  // Pull in dependencies
+  var Backbone = require('backbone');
+  var $        = require('jquery');
+  var AppModel = require('model/app');
+  var AppView  = require('view/app');
+  var TestCollection  = require('collection/test');
+
+  // Create some top level State and UI
+  var app = {
+    model: new AppModel(),
+    view: new AppView()
+  };
+
+  // Create our list of available tests
+  var tests = new TestCollection();
+
+  // Set the tests as a property of our app
+  // model
+  app.model.set('tests', tests);
+
+  // Fetch the list of tests
+  tests.fetch();
+
+  window.app = app;
 });
