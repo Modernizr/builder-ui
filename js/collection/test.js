f965decb1821193144a59bfca4963931cff49681
Alex Sexton <alexsexton@gmail.com>
2013-06-10 22:33:07 -0500

    Some styling for good measure

--- js/collection/test.js
+++ js/collection/test.js
@@ -5,6 +5,14 @@ define(function( require ) {
   return Backbone.Collection.extend({
     name: 'TestCollection',
     model: TestModel,
-    url: './data/metadata.json'
+    url: './data/metadata.json',
+    toJSON: function() {
+      return Backbone.Collection.prototype.toJSON.apply(this.sortBy(function( test ) {
+        if (!test.get('name').match(/^test\//)) {
+          return test.get('name');
+        }
+        return 'zz-' + test.get('name');
+      }), arguments)
+    }
   });
 });
